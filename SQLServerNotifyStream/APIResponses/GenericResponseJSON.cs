471ffef660b8fee0fb24544ca7b3dc81a0e0c67f
Evans R <evansrutoh@gmail.com>
2018-06-28 16:48:48 +0800

    Support for NetwonSoft.JSON added to enable decoding and encoding of JSON req/resp to API

---
+++ SQLServerNotifyStream/APIResponses/GenericResponseJSON.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLServerNotifyStream
+{
+    class GenericResponseJSON
+    {
+        // Hodls the status of the request, can either be success or error
+        public string status { get; set; }
+        // In case of error|success on login, this field is populated with descriptive message
+        public string message { get; set; }
+    }
+}
