e48b0afe8e47c649fcfc52bb959955eb5f3fa150
Evans R <evansrutoh@gmail.com>
2018-06-29 23:47:47 +0800

    - Added JSON decoder for payloads saved to local file system

---
+++ SQLServerNotifyStream/JSONConvert/GenericDatasetJSON.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLServerNotifyStream
+{
+    class GenericDatasetJSON
+    {
+        // Hodls the status of the request, can either be success or error
+        public string itemid { get; set; }
+
+        public string Created_at { get; set; }
+
+        public string qty_ordered { get; set; }
+
+        public string price { get; set; }
+    }
+}
---
+++ SQLServerNotifyStream/JSONConvert/GenericRecordJSON.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLServerNotifyStream
+{
+    class GenericRecordJSON
+    {
+        // Hodls the status of the request, can either be success or error
+        public GenericDatasetJSON dataset { get; set; }
+        
+    }
+}
